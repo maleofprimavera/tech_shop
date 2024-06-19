ba445e75ef35e7dc4867d91cfccadc35cf60aca9
namng <nllsover8.0@gmail.com>
2024-06-19 20:10:59 +0700

    chore: add docker-compose.yml, Dockerfile

--- src/test/java/com/normdevstorm/never_give_up/NeverGiveUpApplicationTests.java
+++ src/test/java/com/normdevstorm/never_give_up/NeverGiveUpApplicationTests.java
@@ -1,9 +1,12 @@
 package com.normdevstorm.never_give_up;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@SpringBootConfiguration
 class NeverGiveUpApplicationTests {
 
 	@Test
